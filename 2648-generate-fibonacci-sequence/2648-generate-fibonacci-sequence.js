764e597058e5e07be5b996ddda01528c8d689255
Lychak Vladilsav <110523693+LychakVlad@users.noreply.github.com>
2023-08-14 21:10:04 -0400

    Time: 44 ms (95.16%), Space: 41.2 MB (97.07%) - LeetHub

--- 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -5,12 +5,12 @@ var fibGenerator = function*() {
   let prev = 0;
   let curr = 1;
 
-  yield prev; // Yield the initial value 0
-  yield curr; // Yield the initial value 1
+  yield prev;
+  yield curr; 
 
   for (let i = 2; i <= 50; i++) {
     const next = prev + curr;
-    yield next; // Yield the next Fibonacci number
+    yield next; 
     prev = curr;
     curr = next;
   }
