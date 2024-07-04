c5158c2758fe8e2338400fb50f78d5a2476f0f29
Lychak Vladislav <110523693+LychakVlad@users.noreply.github.com>
2024-07-03 22:09:35 -0400

    Time: 108 ms (27.38%), Space: 49.4 MB (77.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let indexArr = []
-    for (i =0; i < nums.length; i++) {
-        for (j = i + 1; j < nums.length; j++) {
-                    if (nums[i] + nums[j] === target) {
-            indexArr.push(i)
-            indexArr.push(j)
-               return indexArr
+        for(let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return [i, j]
+                }
+            }
         }
-}
-
     }
-     return indexArr
-};
